7f210026d2aeebdc546126960a977c7805518073
Tomas Jukin <tomas.jukin@juicymo.cz>
2016-11-13 19:38:03 +0100

    Migrate DB with annotate

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id           :integer          not null, primary key
+#  title        :string
+#  text         :text
+#  is_published :boolean
+#  published_at :datetime
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class Article < ActiveRecord::Base
 end
