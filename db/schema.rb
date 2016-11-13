688bb6140ff3975f238acfae1da5af9ea7f83527
Tomas Jukin <tomas.jukin@juicymo.cz>
2016-11-13 19:31:09 +0100

    Migrate DB
    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161113174301) do
+ActiveRecord::Schema.define(version: 20161113182959) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 20161113174301) do
     t.datetime "published_at"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
